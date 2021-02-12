45accbc6fb970faad35f28434b93251ff9cd9846
Huanderson Lima <mattewlima16@gmail.com>
2021-02-12 20:48:22 -0300

    adição de duas colunas a tabela users no banco de dados

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,11 +22,14 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'sexo' => $this->faker->randomElement(['male', 'female']),
+            'data_nasc' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
             'remember_token' => Str::random(10),
         ];
     }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('sexo');
+            $table->date('data_nasc')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
